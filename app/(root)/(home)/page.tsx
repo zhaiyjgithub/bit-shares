abfb92b0a84767c38df135999f6a676b4fa54f8a
yuanjizhai <yuanji.zhai@outlook.com>
2024-06-26 22:39:52 +0800

    project init

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <main>
+            <h1 className={'text-2xl font-bold text-dark-1'}>
+                Welcome to your new project!
+            </h1>
+        </main>
+    )
+}
+export default Page
